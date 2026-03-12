6143cedda2f13b756345b274adda5dc5a83d1aba
claudiacazacu <claudia-mihaela.cazacu@s.unibuc.ro>
2026-03-12 18:05:25 +0200

    added series

--- Main.java
+++ Main.java
@@ -7,6 +7,14 @@ public class Main {
         User u1 = new User("Ana", 20, "ana@mail.com");
         service.addUser(u1);
 
+        Series series = new Series(
+                "Desperate Housewives",
+                "Gossip Girl",
+                "How I Met Your Mother",
+                "Revenge",
+                "And Just Like That"
+        );
+
         Movie movie = new Movie(
                 "Interstellar",
                 "2014",
